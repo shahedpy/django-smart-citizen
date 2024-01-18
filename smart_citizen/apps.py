b2b41d315d663538d1c059cd08b1c1a5f20582e3
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2024-01-18 18:22:16 +0600

    app name

--- smart_citizen/apps.py
+++ smart_citizen/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class YourPackageConfig(AppConfig):
+class SmartCitizenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'smart_citizen'

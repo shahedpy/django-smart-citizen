a4e071dd162b1bafd40c42ff7f1feb9613eec886
Shahed Mohammad Hridoy <35490924+shahedpy@users.noreply.github.com>
2024-08-21 15:10:53 +0600

    Update models.py

--- smart_citizen/models.py
+++ smart_citizen/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class SmartCitizen(models.Model):
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     name_en = models.CharField(
         max_length=255,
         blank=True,
